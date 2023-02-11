088987b2189deb075182e4fc38819e5bf500742a
Philipp Schmitt <philipp@schmitt.co>
2023-02-12 00:42:06 +0100

    Add --cron, --restart-resolver and --tailscale-suffix flags

--- tailscale-hosts.sh
+++ tailscale-hosts.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/env bash
 
 usage() {
-  echo "Usage: $(basename "$0") [--suffix SUFFIX] [-o FILE]"
+  echo "Usage: $(basename "$0") [--cron] [--suffix SUFFIX] [--include-default-suffix] [-o FILE]"
+}
+
+echo_info() {
+  if [[ -n "$CRON" ]]
+  then
+    return 0
+  fi
+
+  echo "🔵 $*" >&2
+}
+
+echo_success() {
+  if [[ -n "$CRON" ]]
+  then
+    return 0
+  fi
+
+  echo "🟢 $*" >&2
+}
+
+echo_error() {
+  # always write error messages to stderr, even when CRON is defined
+  echo "🔴 $*" >&2
+}
+
+is_openwrt() {
+  local ID
+  eval "$(grep -m 1 '^ID=' /etc/os-release)"
+
+  case "$ID" in
+    openwrt*|turris*)
+      return 0
+      ;;
+    *)
+      return 1
+      ;;
+  esac
 }
 
 get_ts_hosts() {
@@ -10,11 +47,46 @@ get_ts_hosts() {
   # Also, we do filter out items that have an empty DNSName which allows us to
   # skip services (eg: hello.ipn.dev)
   tailscale status --json | \
-    jq -er \
-    '[.Self, .Peer[]] |
-      sort_by(.DNSName)[] |
-      select(.DNSName != "") |
-      .TailscaleIPs[0] + " " + ((.DNSName | split("."))[0])'
+    jq -r '[.Self, .Peer[]] |
+           sort_by(.DNSName)[] |
+           select(.DNSName != "") |
+           .TailscaleIPs[0] + " " + ((.DNSName | split("."))[0])'
+}
+
+get_ts_net() {
+   tailscale status --json | jq -er '.MagicDNSSuffix'
+}
+
+generate_hosts_file() {
+  local suffix="$1"
+  local hosts
+  hosts="$(get_ts_hosts)"
+
+  if [[ -z "$hosts" ]]
+  then
+    echo_error "Failed to generate tailscale hosts file"
+    return 1
+  fi
+
+  if [[ -n "$suffix" ]]
+  then
+    # shellcheck disable=2001
+    sed "s/\$/.${suffix}/g" <<< "$hosts"
+  else
+    # No suffix
+    echo "$hosts"
+  fi
+}
+
+restart_resolver_service() {
+  if is_openwrt
+  then
+    echo_info " OpenWRT detected. Restarting resolver service"
+    /etc/init.d/resolver restart
+  else
+    echo_error "Unknown OS. I don't know how to restart the resolver service"
+    return 7
+  fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
@@ -24,11 +96,14 @@ then
   # stdout
   DEFAULT_OUTPUT="/proc/self/fd/1"
   # no suffix by default
-  DEFAULT_SUFFIX=""
+  DEFAULT_SUFFIX="$(get_ts_net)"
 
-  # Use OUTPUT and SUFFIX env values
+  # Initialize vars with env var values
   OUTPUT="${OUTPUT:-${DEFAULT_OUTPUT}}"
   SUFFIX="${SUFFIX:-${DEFAULT_SUFFIX}}"
+  CRON="${CRON:-}"
+  INCLUDE_DEFAULT_SUFFIX="${INCLUDE_DEFAULT_SUFFIX:-}"
+  RESTART_RESOLVER="${RESTART_RESOLVER:-}"
 
   while [[ -n "$*" ]]
   do
@@ -37,9 +112,21 @@ then
         usage
         exit 0
         ;;
+      -c|--cron)
+        CRON=1
+        shift
+        ;;
+      -r|--restart|--restart-resolver)
+        RESTART_RESOLVER=1
+        shift
+        ;;
       -s|--suffix)
         SUFFIX="$2"
         shift 2
+        ;;
+      -t|--tailscale-suffix|--default-suffix|--include-default-suffix|--both)
+        INCLUDE_DEFAULT_SUFFIX=1
+        shift
         ;;
       -o|--output)
         OUTPUT="$2"
@@ -52,27 +139,38 @@ then
     esac
   done
 
-  hosts="$(get_ts_hosts)"
+  NEW_HOSTS=$({
+    if [[ -n "$INCLUDE_DEFAULT_SUFFIX" ]] && \
+       [[ "$SUFFIX" != "$DEFAULT_SUFFIX" ]]
+    then
+      generate_hosts_file "$DEFAULT_SUFFIX"
+    fi
 
-  if [[ -z "$hosts" ]]
+    generate_hosts_file "$SUFFIX"
+  })
+
+  # Check if we are writing to stdout (DEFAULT_OUTPUT)
+  if [[ "$OUTPUT" == "$DEFAULT_OUTPUT" ]]
   then
-    echo "Failed to generate tailscale hosts file" >&2
-    exit 1
+    echo "$HOSTS_FILE" > "$DEFAULT_OUTPUT"
+    exit 0
   fi
 
-  {
-    if [[ -n "$SUFFIX" ]]
-    then
-      # shellcheck disable=2001
-      sed "s/\$/.${SUFFIX}/g" <<< "$hosts"
-    else
-      # No suffix
-      echo "$hosts"
-    fi
-  } > "$OUTPUT"
-
-  if [[ "$OUTPUT" != "$DEFAULT_OUTPUT" ]]
+  # Check if file has changed
+  if diff "$OUTPUT" - <<< "$NEW_HOSTS"
   then
-    echo "Wrote tailscale hosts file to $OUTPUT" >&2
+    echo_success "Hosts file did not change"
+    exit 0
+  fi
+
+  # Write file
+  echo "$NEW_HOSTS" > "$OUTPUT"
+  echo_info "✏️ Wrote tailscale hosts file to $OUTPUT"
+
+  if [[ -n "$RESTART_RESOLVER" ]]
+  then
+    restart_resolver_service
+  else
+    exit 0
   fi
 fi
